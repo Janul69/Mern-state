6ae6986f8e2de59d8d9d1e34b0ae2911540e4076
Janul <januldissanayake99@gmail.com>
2024-01-16 09:36:58 +0530

    create a middleware and a function to handle possible errors.

--- api/index.js
+++ api/index.js
@@ -24,3 +24,13 @@ app.listen(3001, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message || "Internel Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
